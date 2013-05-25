977c76963119676bda710aaa21f623287e25a485
Alex Wolkov <altryne@gmail.com>
2013-05-25 20:10:24 +0300

    added some demo files

--- js/shay.js
+++ js/shay.js
@@ -1,59 +1,60 @@
 angular.module('ShayApp', [])
+	.controller('AppCtrl', function ($scope, $http) {
 
-    .controller('AppCtrl', function ($scope, $http) {
-
-        $("#menu a").on('click',function(e){
-            e.preventDefault();
-            $('html, body').animate({
-                scrollTop: $($(this).attr('href')).offset().top
-            }, 1000);
-        })
-
-
-        $scope.page = 0;
-        $scope.total_pages = 1;
-        $scope.shots = [1, 2, 3];
+		$("#menu a").on('click', function (e) {
+			e.preventDefault();
+			$('html, body').animate({
+				scrollTop: $($(this).attr('href')).offset().top
+			}, 1000);
+		})
 
 
-        $scope.loadMore = function () {
-            if ($scope.page <= $scope.total_pages) {
-                $scope.getShots($scope.page + 1)
-            } else {
-                console.log('nothing more to load, requested page is out of bounds');
-            }
-        };
-        $scope.getNextPage = function(){
-            $scope.page += 1;
-            $scope.getShots($scope.page)
-        }
+		$scope.page = 0;
+		$scope.total_pages = 1;
+		$scope.shots = [1, 2, 3];
 
-        $scope.getShots = function (page,per_page) {
-            per_page = per_page || 4
-            $scope.canLoad = false;
-            $http({
-                url: 'http://api.dribbble.com/players/shayke/shots',
-                method: 'jsonp',
-                params: {
-                    callback: 'JSON_CALLBACK',
-                    per_page: per_page,
-                    page: page || 1
-                }
-            }).then(
-                function (result) {
-                    $scope.canLoad = true;
-                    if ($scope.page == 0) {
-                        $scope.shots = result.data.shots;
-                    } else {
-                        $scope.shots = $scope.shots.concat(result.data.shots);
-                    }
-                    $scope.page = result.data.page;
-                    $scope.total_pages = result.data.pages;
-                }
-            )
-        };
 
-        $scope.getShots(1,11)
-    });
+		$scope.loadMore = function () {
+			if ($scope.page <= $scope.total_pages) {
+				$scope.getShots($scope.page + 1)
+			} else {
+				console.log('nothing more to load, requested page is out of bounds');
+			}
+		};
+
+
+		$scope.getNextPage = function () {
+			$scope.page += 1;
+			$scope.getShots($scope.page)
+		}
+
+		$scope.getShots = function (page, per_page) {
+			per_page = per_page || 4
+			$scope.canLoad = false;
+			$http({
+				url   : 'http://api.dribbble.com/players/shayke/shots',
+				method: 'jsonp',
+				params: {
+					callback: 'JSON_CALLBACK',
+					per_page: per_page,
+					page    : page || 1
+				}
+			}).then(
+					function (result) {
+						$scope.canLoad = true;
+						if ($scope.page == 0) {
+							$scope.shots = result.data.shots;
+						} else {
+							$scope.shots = $scope.shots.concat(result.data.shots);
+						}
+						$scope.page = result.data.page;
+						$scope.total_pages = result.data.pages;
+					}
+			)
+		};
+
+		$scope.getShots(1, 11)
+	});
 
 
 //angular.module('scroll', []).directive('whenScrolled', function () {
